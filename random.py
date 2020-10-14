d4c28c93513b59a171cdb81f5d9556d047220bd4
Chris Filkins <filchyboy@gmail.com>
2020-10-13 19:27:47 -0700

    Added notebook directory

--- random.py
+++ random.py
@@ -1,8 +1,5 @@
-# generate random integer values
-# from numpy.random import seed
-# from numpy.random import randint
-# seed random number generator
-seed(1)
-# generate some integers
-values = randint(0, 10, 20)
-print(values)
+import numpy as np
+
+
+
+print("hello worlld")
